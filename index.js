0f297c52369155b441714ca2ffb6839a505c7521
Touhidur Zaman <touhidcodes@gmail.com>
2023-06-11 09:41:40 +0600

    jwt-api-created

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//  Verify JWT Token
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res
+			.status(401)
+			.send({ error: true, message: "unauthorized access" });
+	}
+	// bearer token
+	const token = authorization.split(" ")[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res
+				.status(401)
+				.send({ error: true, message: "unauthorized access" });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
 
 // MOngoDB Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.57whvd4.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,6 +54,15 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		client.connect();
+
+		// JWT Token API
+		app.post("/jwt", (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+				expiresIn: "1h",
+			});
+			res.send({ token });
+		});
 
 		// Get API of  Class Collection
 		app.get("/classes", async (req, res) => {
@@ -119,7 +150,7 @@ async function run() {
 			const filter = { _id: new ObjectId(id) };
 			const updateUser = {
 				$set: {
-					role: "admin",
+					role: "Admin",
 				},
 			};
 			const result = await usersCollection.updateOne(filter, updateUser);
@@ -133,7 +164,7 @@ async function run() {
 			const filter = { _id: new ObjectId(id) };
 			const updateUser = {
 				$set: {
-					role: "instructor",
+					role: "Instructor",
 				},
 			};
 			const result = await usersCollection.updateOne(filter, updateUser);
